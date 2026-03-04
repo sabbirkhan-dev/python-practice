88f6693e432e45b55293781842d77da80bd723d6
Sabbir <rahabartech@gmail.com>
2026-03-04 17:27:49 +0600

    assignment_01

---
+++ Meta_module/employee_009.py
@@ -0,0 +1,10 @@
+employee_name = "Tuhin Khan"
+age = "22"
+title = "Onwer"
+
+def details():
+    print("Employee name is: ", employee_name)
+    print("Employee age is: ", age)
+    print("Employee title is: ", title)
+
+    
