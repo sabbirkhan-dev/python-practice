b192ee91391bb673b322c07900124c18a4eba32e
Sabbir <rahabartech@gmail.com>
2026-03-04 10:07:29 +0600

    basic_math

---
+++ Meta_module/math_002.py
@@ -0,0 +1,15 @@
+import math
+
+root = math.sqrt(16)
+print("Square Root:", root)
+
+power = math.pow(2, 3)
+print("Power 2^3:", power) 
+
+cosine = math.cos(0)
+print("Cosine of 0:", cosine)
+
+
+degrees = math.cos(math.radians(60))
+print(degrees) 
+
