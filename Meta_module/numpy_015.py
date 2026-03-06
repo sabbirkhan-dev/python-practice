b1bbf92e9971ac7eb263e575653546abb3839e6a
sabbir-kh <rahabartech@gmail.com>
2026-03-06 15:08:56 +0600

    numpy_pandas

--- Meta_module/numpy_015.py
+++ Meta_module/numpy_015.py
@@ -1,13 +1,24 @@
 import numpy as np
+import pandas as pd
+# a = np.zeros(10)
+# print(a)
 
-a = np.zeros(10)
-print(a)
+# b = np.full((2,10), 0.7)
+# print(b)
 
-b = np.full((2,10), 0.7)
-print(b)
+# c = np.linspace(0,25,7)
+# print(c)
 
-c = np.linspace(0,25,7)
-print(c)
+# print(type(c))
 
-print(type(c))
+df = {
+    "Name": ["Rahim", "Karim", "Salam", "Rafi"],
+    "Math": [85, 70, 90, 60],
+    "English": [78, 65, 88, 72]
+}
+
+df = pd.DataFrame(df)
+
+df["Total"] = np.array(df["Math"]) + np.array(df["English"])
+print(df)
 
