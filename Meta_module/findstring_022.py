1a7b419063a34f08281379eea511c3d24d264f81
sabbir-kh <rahabartech@gmail.com>
2026-03-07 23:21:10 +0600

    test_findstring

---
+++ Meta_module/findstring_022.py
@@ -0,0 +1,16 @@
+def ispresent(person):
+    names = ["Sumon", "Joy", "Mannan", "Tutul", "Rana", "Tuhin"]
+    
+    if person in names:
+        return True
+    else:
+        return False
+        
+def nodisit(person):
+    if person.isalpha():
+        return True
+    else:
+        return False
+    
+    
+    
---
+++ Meta_module/test_findstring_021.py
@@ -0,0 +1,11 @@
+import findstring_022
+import pytest
+
+def test_isperson():
+    assert findstring_022.ispresent("Joy")
+    
+    
+# def test_isnodisit():
+    # assert findstring_022.nodisit("N99")
+    
+
